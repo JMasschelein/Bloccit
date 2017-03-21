751290860a85425db8ffa2c12ca3fafdf6ee55ee
JMasschelein <jeroen.masschelein@gmail.com>
2017-03-21 14:21:44 +0000

    checkpoint31

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,6 +2,12 @@ module PostsHelper
     
    def user_is_authorized_for_post?(post)
         current_user && (current_user == post.user || current_user.admin?)
-   end    
+   end
+   
+   def update_rank
+     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
+     new_rank = points + age_in_days
+     update_attribute(:rank, new_rank)
+   end   
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,10 @@ Rails.application.routes.draw do
      resources :comments, only: [:create, :destroy]
    end
    
-  get 'about' => 'welcome#about'
+   post '/up-vote' => 'votes#up_vote', as: :up_vote
+   post '/down-vote' => 'votes#down_vote', as: :down_vote   
+   
+   get 'about' => 'welcome#about'
 
    root 'welcome#index'
    
