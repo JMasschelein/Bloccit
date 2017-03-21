84fde22decc02e3906b8c00f21b29183aeed574e
JMasschelein <jeroen.masschelein@gmail.com>
2017-03-21 13:52:50 +0000

    checkpoint29

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post, optional: true
+   belongs_to :user
+ 
+   validates :body, length: { minimum: 5 }, presence: true
+   validates :user, presence: true  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ require 'random_data'
  # Create Comments
  100.times do
    Comment.create!(
- # #4
+     user: users.sample,
      post: posts.sample,
      body: RandomData.random_paragraph
    )
