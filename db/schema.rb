2254942ce061d4de44c84df3ca6bdc9019d3f1ff
JMasschelein <jeroen.masschelein@gmail.com>
2017-03-22 13:56:02 +0000

    checkpoint28Assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321142213) do
+ActiveRecord::Schema.define(version: 20170322131853) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 20170321142213) do
     t.float    "rank"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
