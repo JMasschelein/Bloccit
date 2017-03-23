8c89c515174f316b9e4653edb0c2d6554e509b76
JMasschelein <jeroen.masschelein@gmail.com>
2017-03-23 13:25:10 +0000

    checkpoint30B

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,8 @@ require 'random_data'
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
-   
+   # post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+   # rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }   
 
    
  end
