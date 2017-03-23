7b2f9394dc09fa17837a0cee8822197fb56c5838
JMasschelein <jeroen.masschelein@gmail.com>
2017-03-23 16:56:48 +0000

    checkpoint30

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
     has_many :favorites, dependent: :destroy
+    
+    after_create:create_vote
 
     
     default_scope { order('rank DESC') }
@@ -30,6 +32,11 @@ class Post < ApplicationRecord
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+   
+   private
+    def create_vote
+        user.votes.create(value:1, post:self)
    end
    
 end
