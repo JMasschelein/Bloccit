8403930765b39925c73bd0c566ff7a49817f76be
JMasschelein <jeroen.masschelein@gmail.com>
2017-03-30 18:44:29 +0000

    checkpoint31Unfinished

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'tranquil-fortress-92560.herokuapp.com' }
 end
